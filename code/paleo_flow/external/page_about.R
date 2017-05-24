e00c47f688455e7fc6f716a04f16f228c3105fbf
JamesStagge <jhstagge@gmail.com>
2017-05-24 16:11:41 -0600

    Fixed the About page

--- code/paleo_flow/external/page_about.R
+++ code/paleo_flow/external/page_about.R
@@ -3,21 +3,46 @@
 ###########################################################################           
 
 column(9,
-	column(12,
-	h2("About"),
-        	p("The Reconstructed Streamflow Explorer was developed by James Stagge, in conjunction with the Utah State University Water Lab. It was funded in part by Utah Mineral Lease funds"),
-        	br(),
-        	br(),
-        	h3("Citation"),
-        	p("When using the Reconstructed Streamflow Explorer for research or background, please cite as follows:"),
-        	p("Stagge, J.H. (2017) Reconstructed Streamflow Explorer. www.com"),
-        	br(),
-            tags$p("All code for the Reconstructed Streamflow Explorer is available open source on GitHub. Feel free to contribute to this project there: ", tags$a(href="https://github.com/jstagge/paleo_flow_shiny", "https://github.com/jstagge/paleo_flow_shiny"), ".")	,
-        	br(),
-        	br(),
-        	h3("Contact Information"),
+	column(12,fluidRow(h2("About"))),
+	
+	column(7,
+		fluidRow(
+			tags$p("The Reconstructed Streamflow Explorer was developed by ", tags$a(href="www.jstagge.com", "James Stagge"), "in conjunction with the ", tags$a(href="https://uwrl.usu.edu/", "Utah State University Water Research Lab"), " and the ", tags$a(href="https://wadr.usu.edu/", "Wasatch Dendroclimatology Research Group") ,". It was funded in part by Utah Mineral Lease funds.")
+		),
+       	fluidRow(
+       		h3("Citation"),
+       		p("When using the Reconstructed Streamflow Explorer for research or reference, please cite as follows:")
+       	),
+       	fluidRow(
+       		strong("Stagge, J.H. (2017) PaleoFlow Reconstructed Streamflow Explorer Version 2.0.0. www.paleoflow.org  doi:10.5281/zenodo.570935"),
+       		
+			tags$div(tags$a(href="https://github.com/jstagge/paleo_flow_shiny",  img(src="https://zenodo.org/badge/DOI/10.5281/zenodo.570935.svg", alt="DOI"))),
+		 	br()
+		 ),
+		 fluidRow(
+		 br(),
+		   tags$p("All code for this application is available as a", tags$a(href="https://github.com/jstagge/paleo_flow_shiny", "GitHub repository"), ", made available under the MIT license. ")
+		 ),
+       	fluidRow(
+       		h3("Contact Information"),
         	tags$p("Please direct any questions to ", tags$a(href="mailto:james.stagge@usu.edu", "James Stagge"), ".")
-        
-   )         
+		)		
+		
+	), 
+	column(3, offset=1,
+			fluidRow(
+			tags$div(img(src = "img/Utah_State_University_Logo.png", class="img-responsive")),
+			br()
+			),
+			fluidRow(
+			tags$div(img(src = "img/uwrllogo.gif", class="img-responsive")),
+			br()
+			), 
+			fluidRow(
+			tags$div(img(src = "img/wadr.jpg", class="img-responsive")),
+			br()
+			)
+	)
+    	
+)		
 
-)
